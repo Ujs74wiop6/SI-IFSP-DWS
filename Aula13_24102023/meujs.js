47441c42197b6e0d6c5755dbc5b3cf42bc43a4bd
Ujs74wiop6 <fabricioujs74@gmail.com>
2023-10-24 16:51:39 -0300

    update 24/10/2023

---
+++ Aula13_24102023/meujs.js
@@ -0,0 +1,10 @@
+/*
+
+Aula 01 JavaScript
+
+*/
+
+console.log("Olá Mundo") /* Console */
+alert("Olá Mundo") /* Alerta */
+prompt("Qual é o seu nome?")
+confirm("Você sabe programar em JavaScript")
